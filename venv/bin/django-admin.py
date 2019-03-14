9cc43be436f51dd42d7ca89946ab2cacb717b490
“Nabila” <nurnabilarzar@gmail.com>
2019-03-14 12:32:44 +0800

    3rd

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,6 @@
+#!/home/nabila/python/venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.0.dev20190313142428','django-admin.py'
+__requires__ = 'Django==3.0.dev20190313142428'
+__import__('pkg_resources').require('Django==3.0.dev20190313142428')
+__file__ = '/home/nabila/python/django/django/bin/django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
